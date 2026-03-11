5f59dab69f6c7e02fd72a1e3445a58d8f86c8f55
xu zhoux <1040279302@qq.com>
2026-03-11 15:56:38 +0800

    配置环境、创建导航栏

---
+++ frontend/src/components/navbar/NarBar.vue
@@ -0,0 +1,78 @@
+<script setup>
+
+import MenuIcon from "@/components/navbar/icons/MenuIcon.vue";
+import HomepageIcon from "@/components/navbar/icons/HomepageIcon.vue";
+import FriendIcon from "@/components/navbar/icons/FriendIcon.vue";
+import CreateIcon from "@/components/navbar/icons/CreateIcon.vue";
+import SearchIcon from "@/components/navbar/icons/SearchIcon.vue";
+</script>
+
+<template>
+  <div class="drawer lg:drawer-open">
+    <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <nav class="navbar w-full bg-base-100 shadow-sm">
+        <div class="navbar-start">
+          <label for="my-drawer-4" aria-label="open sidebar" class="btn btn-square btn-ghost">
+            <MenuIcon />
+          </label>
+          <div class="px-2 font-bold text-2xl">AIFriends</div>
+        </div>
+
+        <div class="navbar-center w-4/5 max-w-180">
+          <div class="join w-4/5">
+            <input class="input join-item w-4/5 rounded-l-full" placeholder="搜索你感兴趣的内容" />
+            <button class="btn join-item rounded-r-full gap-0.5">
+              <SearchIcon /> 搜索
+            </button>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <button class="btn btn-ghost text-lg">登录</button>
+        </div>
+
+      </nav>
+      <slot></slot>
+    </div>
+
+    <div class="drawer-side is-drawer-close:overflow-visible">
+      <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
+      <div class="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-16 is-drawer-open:w-54">
+        <ul class="menu w-full grow">
+          <li>
+            <button
+              class="py-3 px-3 rounded text-gray-700 hover:text-blue-500 hover:bg-gray-100 transition cursor-pointer"
+              data-tip="首页">
+              <homepage-icon />
+              <span class="is-drawer-close:hidden text-base ml-2 whitespace-nowrap">首页</span>
+            </button>
+          </li>
+
+          <li>
+            <button
+              class="py-3 px-3 rounded text-gray-700 hover:text-blue-500 hover:bg-gray-100 transition cursor-pointer"
+              data-tip="好友">
+              <friend-icon />
+              <span class="is-drawer-close:hidden text-base ml-2 whitespace-nowrap">好友</span>
+            </button>
+          </li>
+
+          <li>
+            <button
+              class="py-3 px-3 rounded text-gray-700 hover:text-blue-500 hover:bg-gray-100 transition cursor-pointer"
+              data-tip="创作">
+              <create-icon />
+              <span class="is-drawer-close:hidden text-base ml-2 whitespace-nowrap">创作</span>
+            </button>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
